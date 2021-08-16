c3e306a3eea640591b6d360723cf4b9398345c66
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-08-16 12:01:16 +0530

    error handling

---
+++ block-BNaabu/sample/server.js
@@ -0,0 +1,34 @@
+var express = require("express")
+var logger = require('morgan')
+var app = express();
+
+app.use(logger('dev'));
+
+app.use((req, res, next) => {
+    if (req.url === "/admin") {
+      return next("Unauthorized");
+    }
+    next();
+  });
+
+
+app.get('/' , (req,res) => {
+    res.send('Welcome')
+})
+
+app.get('/about', (req,res) => {
+    res.send('Users Page')
+})
+
+app.use((req,res,next) =>{
+    res.send("page not found")
+})
+
+app.use((err,req,res,next) => {
+    console.log(err)
+    res.send(err)
+})
+
+app.listen(3000 , () => {
+    console.log("server is listen on 3k")
+})
