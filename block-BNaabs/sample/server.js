979e8d3b4d5be02d3cad5337ee366d11c405f3b0
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-08-16 11:02:00 +0530

    refactored

--- block-BNaabs/sample/server.js
+++ block-BNaabs/sample/server.js
@@ -2,10 +2,23 @@ var express = require('express')
 var logger = require('morgan')
 var app = express();
 
-
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended : false}))
+// app.use(express.static(__dirname + "/sample"))
 app.use(logger('dev'))
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname + "/index.html") 
+})
+
+app.get('/new' , (req,res) => {
+    res.sendFile(__dirname + "/new.html")
+})
+
+app.post("/new" , (req,res) => {
+    console.log(req.body)
+    res.json(req.body)
 })
 
 app.get('/users/:username' , (req,res) => {
